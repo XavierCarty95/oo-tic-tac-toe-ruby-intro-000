353676baf5681f580b010541af382b80d89c6840
Xavier Carty <xaviercarty@gmail.com>
2019-11-22 03:39:32 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,7 @@ class TicTacToe
   attr_accessor :board
 
   def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    @board = Array.new(9, "")
   end
 
   WIN_COMBINATIONS = [
